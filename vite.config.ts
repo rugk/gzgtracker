738237c20b4e8d8d86bf3c8d094dc349f0ed1164
rugk <rugk+git@posteo.de>
2026-04-22 18:38:19 +0200

    style: format file

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [vue()],
-  test: {
-    environment: 'happy-dom',
-  },
+    test: {
+        environment: 'happy-dom',
+    },
 });
