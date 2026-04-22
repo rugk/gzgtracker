5d2965bbac9ef9b95ab73e83765913276b05d2c2
rugk <rugk+git@posteo.de>
2026-04-22 18:39:31 +0200

    fix(ci): set base for deploymnent

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    base: "/gzgtracker/",
     plugins: [vue()],
     test: {
         environment: 'happy-dom',
